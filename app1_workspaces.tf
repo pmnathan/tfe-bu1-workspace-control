12fe278283375b55eee1cc1b7f85f19c45028e74
Prakash Manglanathan <pmnathan@gmail.com>
2021-06-18 11:00:02 -0400

    Update app1_workspaces.tf

--- app1_workspaces.tf
+++ app1_workspaces.tf
@@ -12,3 +12,34 @@ resource "tfe_workspace" "bu1-app1-aws-useast1-dev" {
     oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
   }
 }
+
+resource "tfe_workspace" "bu1-app1-aws-useast1-test" {
+  name           = "bu1-app1-aws-useast1-dev"
+  organization   = "prakash_demo_tfc_business"
+  execution_mode = "remote"
+  vcs_repo   {
+    identifier     = "pmnathan/tfworkshopdemo"
+    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+  }
+}
+
+
+resource "tfe_workspace" "bu1-app1-aws-useast1-prod" {
+  name           = "bu1-app1-aws-useast1-dev"
+  organization   = "prakash_demo_tfc_business"
+  execution_mode = "remote"
+  vcs_repo   {
+    identifier     = "pmnathan/tfworkshopdemo"
+    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+  }
+}
+
+resource "tfe_workspace" "bu1-app1-aws-useast2-prod" {
+  name           = "bu1-app1-aws-useast1-dev"
+  organization   = "prakash_demo_tfc_business"
+  execution_mode = "remote"
+  vcs_repo   {
+    identifier     = "pmnathan/tfworkshopdemo"
+    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+  }
+}
