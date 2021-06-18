036a4692a259033bd404ba4a413b1c01eb34201e
Prakash Manglanathan <pmnathan@gmail.com>
2021-06-18 10:32:47 -0400

    Update app1_workspaces.tf

--- app1_workspaces.tf
+++ app1_workspaces.tf
@@ -2,4 +2,5 @@ resource "tfe_workspace" "bu1-app1-aws-useast1-dev" {
   name           = "bu1-app1-aws-useast1-dev"
   organization   = "prakash_demo_tfc_business"
   execution_mode = "remote"
+  vcs_repo       = "pmnathan/tfworkspacedemo"
 }
