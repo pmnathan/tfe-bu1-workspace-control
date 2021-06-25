cddec9e6de12353715cebf5966a9f64bec54de62
Prakash Manglanathan <pmnathan@gmail.com>
2021-06-24 21:48:13 -0400

    Update app1_workspaces.tf

--- app1_workspaces.tf
+++ app1_workspaces.tf
@@ -1,48 +1,42 @@
+variable "org_name" {
+  default     = "demo-rbac"
+}
+
 data "tfe_oauth_client" "client" {
   oauth_client_id = "oc-hd7uSxBZW61jiwVp"
 }
 
-/*
+data "tfe_team" "bu1admin" {
+  name         = "bu1admin"
+  organization = var.org_name
+}
+
+
 resource "tfe_workspace" "bu1-app1-aws-useast1-dev" {
   name           = "bu1-app1-aws-useast1-dev"
-  organization   = "prakash_demo_tfc_business"
+  organization   =  var.org_name
   execution_mode = "remote"
-  vcs_repo   {
-    identifier     = "pmnathan/tfworkshopdemo"
-    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
-  }
 }
-*/
-/*
+
+
+
 resource "tfe_workspace" "bu1-app1-aws-useast1-test" {
   name           = "bu1-app1-aws-useast1-test"
-  organization   = "prakash_demo_tfc_business"
+  organization   = var.org_name
   execution_mode = "remote"
-  vcs_repo   {
-    identifier     = "pmnathan/tfworkshopdemo"
-    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
-  }
 }
 
 
 resource "tfe_workspace" "bu1-app1-aws-useast1-prod" {
   name           = "bu1-app1-aws-useast1-prod"
-  organization   = "prakash_demo_tfc_business"
+  organization   = var.org_name
   execution_mode = "remote"
-  vcs_repo   {
-    identifier     = "pmnathan/tfworkshopdemo"
-    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
-  }
 }
 
 resource "tfe_workspace" "bu1-app1-aws-useast2-prod" {
   name           = "bu1-app1-aws-useast2-prod"
-  organization   = "prakash_demo_tfc_business"
+  organization   = var.org_name
   execution_mode = "remote"
-  vcs_repo   {
-    identifier     = "pmnathan/tfworkshopdemo"
-    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
-  }
 }
 
-*/
+
