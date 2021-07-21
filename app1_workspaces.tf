835edf9fdd54d214a431b716b0897181e97ec66b
Prakash Manglanathan <pmnathan@gmail.com>
2021-07-20 23:05:26 -0400

    Update app1_workspaces.tf

--- app1_workspaces.tf
+++ app1_workspaces.tf
@@ -2,6 +2,18 @@ variable "org_name" {
   default     = "demo-rbac"
 }
 
+variable "aws_key_id" {
+  description = "AWS Key ID"
+}
+
+variable "aws_key_secret" {
+  description = "AWS SECRET"
+  default     = "us-east-2"
+  sensitive   = true
+}
+
+
+
 data "tfe_oauth_client" "client" {
   oauth_client_id = "oc-hd7uSxBZW61jiwVp"
 }
@@ -16,11 +28,28 @@ data "tfe_team" "bu1dev" {
   organization = var.org_name
 }
 
+
 resource "tfe_workspace" "bu1-app1-aws-useast1-dev" {
   name           = "bu1-app1-aws-useast1-dev"
   organization   =  var.org_name
   execution_mode = "remote"
 }
+
+resource "tfe_variable" "bu1-app1-aws-useast1-dev-awskey" {
+  key          = "AWS_KEY_ID"
+  value        = var.aws_key_id
+  category     = "terraform"
+  workspace_id = tfe_workspace.bu1-app1-aws-useast1-dev.id
+}
+
+resource "tfe_variable" "bu1-app1-aws-useast1-dev-awssecret" {
+  key          = "AWS_SECRET"
+  value        = var.aws_key_secret
+  category     = "terraform"
+  workspace_id = tfe_workspace.bu1-app1-aws-useast1-dev.id
+  sensitive    = true
+}
+
 
 resource "tfe_team_access" "bu1-app1-aws-useast1-dev-accessadmin" {
   access       = "admin"
